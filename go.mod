ee8a88f41606d5f8eeff486bbd18b7bdcb009885
8treenet <4932004@qq.com>
2021-06-03 18:45:16 +0800

    mongo infra

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/lestrrat-go/strftime v1.0.4 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0 // indirect
+	go.mongodb.org/mongo-driver v1.5.3
 	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gorm.io/driver/mysql v1.0.6
---
+++ infra/mongo.go
@@ -0,0 +1,111 @@
+package infra
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/8treenet/freedom"
+	driver "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+// main _"github.com/8treenet/crm-service/infra"
+func init() {
+	freedom.Prepare(func(initiator freedom.Initiator) {
+		initiator.BindInfra(true, &Mongo{})
+		initiator.InjectController(func(ctx freedom.Context) (com *Mongo) {
+			initiator.FetchInfra(ctx, &com)
+			return
+		})
+	})
+}
+
+// Mongo .
+type Mongo struct {
+	freedom.Infra
+	db            *driver.Client
+	opts          *options.ClientOptions
+	databaseName  string
+	database      *driver.Database
+	collectionMap map[string]*driver.Collection
+	mutex         *sync.Mutex
+}
+
+// Start .
+func (mongo *Mongo) visitConfig() {
+	var mongoConf struct {
+		Database string `toml:"database"`
+		URI      string `toml:"uri"`
+	}
+	//default
+	mongoConf.Database = "freedom"
+	mongoConf.URI = "mongodb://root:123123@localhost:27017"
+
+	freedom.Configure(&mongoConf, "mongo.toml")
+	mongo.opts = options.Client().ApplyURI(mongoConf.URI)
+	mongo.databaseName = mongoConf.Database
+}
+
+// Booting .
+func (mongo *Mongo) Booting(bootManager freedom.BootManager) {
+	mongo.visitConfig()
+
+	ctx, fun := context.WithTimeout(context.Background(), time.Second*2)
+	defer fun()
+	client, err := driver.Connect(ctx, mongo.opts)
+	if err != nil {
+		freedom.Logger().Fatal(err)
+	}
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		freedom.Logger().Fatal(err)
+	}
+
+	bootManager.RegisterShutdown(func() {
+		client.Disconnect(context.TODO())
+	})
+
+	mongo.db = client
+	mongo.database = mongo.db.Database(mongo.databaseName)
+	freedom.Logger().Infof("Connect mongo success, database:%s.", mongo.databaseName)
+}
+
+func (mongo *Mongo) GetCollection(data interface{}) *driver.Collection {
+	type getCollection interface {
+		Collection() string
+	}
+
+	collectionName := fmt.Sprint(data)
+	impl, ok := data.(getCollection)
+	if ok {
+		collectionName = impl.Collection()
+	}
+
+	mongo.mutex.Lock()
+	defer mongo.mutex.Unlock()
+
+	if result, ok := mongo.collectionMap[collectionName]; ok {
+		return result
+	}
+
+	result := mongo.database.Collection(collectionName)
+	mongo.collectionMap[collectionName] = result
+	return result
+}
+
+func (mongo *Mongo) ToDocuments(data interface{}) (documents []interface{}) {
+	value := reflect.ValueOf(data)
+	if value.Type().Kind() != reflect.Slice && value.Type().Kind() != reflect.Array {
+		freedom.Logger().Error("ToDocuments:传入类型data错误,必须是一个数组.")
+		return
+	}
+
+	for i := 0; i < value.Len(); i++ {
+		documents = append(documents, value.Index(i).Interface())
+	}
+	return
+}
